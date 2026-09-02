ed3b30b669a7522e3be17acba3fdda0a7e641a6f
faridsultan929-del <faridsultan929@gmail.com>
2026-09-02 15:02:25 +0400

    Update MainActivity.java

--- app/src/main/java/com/workerpro/v3/MainActivity.java
+++ app/src/main/java/com/workerpro/v3/MainActivity.java
@@ -1,4 +1,14 @@
 package com.example.workerpro;
 
-public class MainActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
 }
