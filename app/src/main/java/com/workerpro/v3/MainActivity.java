a825f29250d31c786a407fe669680f11b16077e7
faridsultan929-del <faridsultan929@gmail.com>
2026-09-02 20:23:43 +0400

    Update MainActivity.java

--- app/src/main/java/com/workerpro/v3/MainActivity.java
+++ app/src/main/java/com/workerpro/v3/MainActivity.java
@@ -2,6 +2,8 @@ package com.workerpro.v3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,42 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button safety = findViewById(R.id.btnSafety);
+        Button manufacturing = findViewById(R.id.btnManufacturing);
+        Button stamping = findViewById(R.id.btnStamping);
+        Button furnaces = findViewById(R.id.btnFurnaces);
+        Button fire = findViewById(R.id.btnFire);
+        Button sensors = findViewById(R.id.btnSensors);
+        Button welding = findViewById(R.id.btnWelding);
+        Button quality = findViewById(R.id.btnQuality);
+        Button english = findViewById(R.id.btnEnglish);
+
+        safety.setOnClickListener(v ->
+                Toast.makeText(this, "SAFETY", Toast.LENGTH_SHORT).show());
+
+        manufacturing.setOnClickListener(v ->
+                Toast.makeText(this, "MANUFACTURING", Toast.LENGTH_SHORT).show());
+
+        stamping.setOnClickListener(v ->
+                Toast.makeText(this, "STAMPING", Toast.LENGTH_SHORT).show());
+
+        furnaces.setOnClickListener(v ->
+                Toast.makeText(this, "FURNACES", Toast.LENGTH_SHORT).show());
+
+        fire.setOnClickListener(v ->
+                Toast.makeText(this, "FIRE EXTINGUISHERS", Toast.LENGTH_SHORT).show());
+
+        sensors.setOnClickListener(v ->
+                Toast.makeText(this, "SENSORS", Toast.LENGTH_SHORT).show());
+
+        welding.setOnClickListener(v ->
+                Toast.makeText(this, "WELDING", Toast.LENGTH_SHORT).show());
+
+        quality.setOnClickListener(v ->
+                Toast.makeText(this, "QUALITY CONTROL", Toast.LENGTH_SHORT).show());
+
+        english.setOnClickListener(v ->
+                Toast.makeText(this, "ENGLISH FOR WORKERS", Toast.LENGTH_SHORT).show());
     }
 }
